c343fd123f97e87abbf83be16eb42c7f1b585af1
joeriddles <joeriddles10@gmail.com>
2024-09-03 15:49:15 -0700

    Fix not showing verses in lists and add caching in localStorage

--- main.ts
+++ main.ts
@@ -20,77 +20,94 @@ export default class BibleVerseQuicklookPlugin extends Plugin {
 
 		// TODO: refactor this big boy
 		this.registerMarkdownPostProcessor(async (element, context) => {
-			const lines = element.findAll("p")
-			for (const line of lines) {
-				const html = line.innerHTML;
-				const matches = [...html.matchAll(VERSE_PATTERN)]
-				if (matches) {
-					const newLine = createEl("p")
+			const elems = ["p", "li"]
 
-					let end = -1;
-					let prevEnd = -1;
+			for (const elem of elems) {
+				const lines = element.findAll(elem)
+				for (const line of lines) {
+					const html = line.innerHTML;
+					const matches = [...html.matchAll(VERSE_PATTERN)]
+					if (matches) {
 
-					for (const match of matches) {
-						const start = match.index ?? 0
-						end = start + match[0].length
-						const text = html.slice(start, end)
+						// Cheating here to bypass HTMLElementTagNameMap requirement:
+						// eslint-disable-next-line @typescript-eslint/no-explicit-any
+						const newElem = createEl(elem as any)
 
-						if (!match.groups) {
-							throw new Error(`impossible: ${line}`)
-						}
+						let end = -1;
+						let prevEnd = -1;
 
-						const bookAbbreviation = match.groups.book
-						const book = bookService.lookupBookByAbbreviation(bookAbbreviation)
-						if (book == null) {
-							console.warn(`Could not find matching book for abbreviation: ${bookAbbreviation}`)
-							continue
-						}
+						for (const match of matches) {
+							const start = match.index ?? 0
+							end = start + match[0].length
+							const text = html.slice(start, end)
 
-						const verse = {
-							book,
-							chapter: Number.parseInt(match.groups.chapter),
-							verseStart: Number.parseInt(match.groups.verseStart),
-							verseEnd: match.groups.verseEnd ? Number.parseInt(match.groups.verseEnd) : undefined,
-						} as Verse
-						console.log(verse)
-
-						newLine.innerHTML += html.substring(prevEnd, start)
-						newLine.dataset.verse = JSON.stringify(book)
-						prevEnd = end
-						const onmouseover = `if (this.dataset.loaded !== "true") {
-	this.dataset.loaded = "true";
-	${verseService.getLookVerseString(verse)}
-	const verse = JSON.parse(this.dataset.verse);
-	lookupVerse(verse).then(title => this.title = title);
-}`
-						newLine.createSpan({
-							text,
-							attr: {
-								"style": "text-decoration: underline dotted;",
-								"title": "Loading...",
-								"data-loaded": "false",
-								"data-verse": JSON.stringify({ verse }),
-								"onmouseover": onmouseover,
+							if (!match.groups) {
+								throw new Error(`impossible: ${line}`)
 							}
-						}
-							// TODO: this does not work...
-							// , (span) => {
-							// 	span.onmouseover = () => {
-							// 		console.log("onmouseover")
-							// 		if (span.dataset.loaded !== "true") {
-							// 			span.dataset.loaded = "true"
-							// 			verseService.lookupVerse(verse).then(title => {
-							// 				span.title = title
-							// 				console.log(title)
-							// 			})
-							// 		}
-							// 	}
-							// }
-						)
-					}
 
-					newLine.innerHTML += html.substring(end)
-					line.replaceWith(newLine)
+							const bookAbbreviation = match.groups.book
+							const book = bookService.lookupBookByAbbreviation(bookAbbreviation)
+							if (book == null) {
+								console.warn(`Could not find matching book for abbreviation: ${bookAbbreviation}`)
+								continue
+							}
+
+							const verse = {
+								book,
+								chapter: Number.parseInt(match.groups.chapter),
+								verseStart: Number.parseInt(match.groups.verseStart),
+								verseEnd: match.groups.verseEnd ? Number.parseInt(match.groups.verseEnd) : undefined,
+							} as Verse
+							console.log(verse)
+
+							newElem.innerHTML += html.substring(prevEnd, start)
+							newElem.dataset.verse = JSON.stringify(book)
+							prevEnd = end
+							const onmouseover = `
+if (this.dataset.loaded !== "true") {
+	const verse = JSON.parse(this.dataset.verse);
+	const title = localStorage.getItem("bible-verse-quicklook:${verse.book}:${verse.chapter}:${verse.verseStart}")
+	if (title != null) {
+		this.dataset.loaded = "true";
+		this.title = title;
+	} else {
+		this.dataset.loaded = "true";
+		${verseService.getLookVerseString(verse)}
+		lookupVerse(verse).then(title => {
+			this.title = title;
+			localStorage.setItem("bible-verse-quicklook:${verse.book}:${verse.chapter}:${verse.verseStart}", title);
+		});
+	}
+}`
+							newElem.createSpan({
+								text,
+								attr: {
+									"style": "text-decoration: underline dotted;",
+									"title": "Loading...",
+									"data-loaded": "false",
+									"data-verse": JSON.stringify({ verse }),
+									"onmouseover": onmouseover,
+								}
+							}
+								// TODO: this does not work...
+								// , (span) => {
+								// 	span.onmouseover = () => {
+								// 		console.log("onmouseover")
+								// 		if (span.dataset.loaded !== "true") {
+								// 			span.dataset.loaded = "true"
+								// 			verseService.lookupVerse(verse).then(title => {
+								// 				span.title = title
+								// 				console.log(title)
+								// 			})
+								// 		}
+								// 	}
+								// }
+							)
+						}
+
+						newElem.innerHTML += html.substring(end)
+						line.replaceWith(newElem)
+					}
 				}
 			}
 		})
